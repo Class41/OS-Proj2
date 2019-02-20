26f40ff9a9903b620ef6ddd16c7c13c63c44a60b
Vasyl Onufriyev <projectclass41@gmail.com>
2019-02-19 23:32:06 -0600

    shm test

--- oss.c
+++ oss.c
@@ -10,9 +10,25 @@
 void DoSharedWork(char* filename, int childMax, int childConcurMax, FILE* input, FILE* output)
 {
 
+    // ftok to generate unique key 
+    key_t key = ftok("shmfile",65); 
+  
+    // shmget returns an identifier in shmid 
+    int shmid = shmget(key,1024,0666|IPC_CREAT); 
+  
+    // shmat to attach to shared memory 
+    char *str = (char*) shmat(shmid,(void*)0,0); 
+  
+    cout<<"Write Data : "; 
+    gets(str); 
+  
+    printf("Data written in memory: %s\n",str); 
+      
+    //detach from shared memory  
+    shmdt(str); 
+  
 
-
-	key_t shmkey = ftok("shmshare", 65);
+	/*key_t shmkey = ftok("shmshare", 65);
 
 	int ipcid = shmget(shmkey, 85 * sizeof(Shared), 0666|IPC_CREAT);
 
@@ -20,7 +36,7 @@ void DoSharedWork(char* filename, int childMax, int childConcurMax, FILE* input,
 	
 	data->seconds = 5;
 
-	shmdt(data);
+	shmdt(data);*/
 }
 
 int main(int argc, char** argv)
